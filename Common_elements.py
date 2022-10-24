deda4adaff3cd132f378ecff82eb3f89c869b9c8
bikkinahema <102247468+bikkinahema@users.noreply.github.com>
2022-10-24 21:20:07 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) l=[] c=0 for i in a:     for j in b:         if j==i:             if i not in l:                 c+=1                 l.append(i) print(*l)

---
+++ Common_elements.py
@@ -0,0 +1,13 @@
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+l=[]
+c=0
+for i in a:
+    for j in b:
+        if j==i:
+            if i not in l:
+                c+=1
+                l.append(i)
+print(*l)
+            
