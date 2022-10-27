9ecee339de3f9430e3c37d0d7f5f0e7ce6e177ca
bikkinahema <102247468+bikkinahema@users.noreply.github.com>
2022-10-27 23:31:45 +0530

    n=input() w=n.split() m=0 for j in w:     c=0     for k in j:         s=' '         if k not in s:             if k=='a' or k=='A':                 c+=1                 s+=k         if k not in s:             if k=='e' or k=='E':                 c+=1                 s+=k         if k not in s:             if k=='i' or k=='I':                 c+=1                 s+=k         if k not in s:             if k=='o' or k=='O':                 c+=1                 s+=k         if k not in s:             if k=='u' or k=='U':                 c+=1                 s+=k     if c>m:         m=c print(m)

---
+++ maximum_vowel_count.py
@@ -0,0 +1,31 @@
+n=input()
+w=n.split()
+m=0
+for j in w:
+    c=0
+    for k in j:
+        s=' '
+        if k not in s:
+            if k=='a' or k=='A':
+                c+=1
+                s+=k
+        if k not in s:
+            if k=='e' or k=='E':
+                c+=1
+                s+=k
+        if k not in s:
+            if k=='i' or k=='I':
+                c+=1
+                s+=k
+        if k not in s:
+            if k=='o' or k=='O':
+                c+=1
+                s+=k
+        if k not in s:
+            if k=='u' or k=='U':
+                c+=1
+                s+=k
+    if c>m:
+        m=c
+print(m)
+        
